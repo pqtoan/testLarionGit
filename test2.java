325ddc0b8b3991a4d45edf5ce105dc191a4c6da3
MrToan <toanpq@elarion.com>
2014-10-22 11:20:14 +0700

    Revert "adding class "bank""
    This reverts commit 42b7befb4bd3cc79609bc21b96685da2b9e4d47f.

--- test2.java
+++ test2.java
@@ -1,15 +1,5 @@
 import java.util.*;
 
-class bank
-{
-	int id;
-	int balance;
-	public bank(int x, int y)
-	{
-		id = bank;
-		balance = y;
-	}
-}
 public class test2
 {
 	public static void main(String args[])
